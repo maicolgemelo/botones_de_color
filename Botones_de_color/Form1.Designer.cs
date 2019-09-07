85de84865cab4148f6e65e2e1e70dc4839e0ca78
michael escobar galeano <maicolgemelo@gmail.com>
2019-09-06 19:01:47 -0500

    programa que cambia el color del fondo de la interfaz con un boton

---
+++ Botones_de_color/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Botones_de_color
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitular = new System.Windows.Forms.Label();
+            this.btnAzul = new System.Windows.Forms.Button();
+            this.btnVerde = new System.Windows.Forms.Button();
+            this.btnRojo = new System.Windows.Forms.Button();
+            this.btnNaranja = new System.Windows.Forms.Button();
+            this.btnNegro = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitular
+            // 
+            this.lblTitular.AutoSize = true;
+            this.lblTitular.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitular.Location = new System.Drawing.Point(214, 26);
+            this.lblTitular.Name = "lblTitular";
+            this.lblTitular.Size = new System.Drawing.Size(312, 35);
+            this.lblTitular.TabIndex = 0;
+            this.lblTitular.Text = "Cambio color de fondo";
+            // 
+            // btnAzul
+            // 
+            this.btnAzul.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAzul.Location = new System.Drawing.Point(658, 92);
+            this.btnAzul.Name = "btnAzul";
+            this.btnAzul.Size = new System.Drawing.Size(75, 32);
+            this.btnAzul.TabIndex = 1;
+            this.btnAzul.Text = "Azul";
+            this.btnAzul.UseVisualStyleBackColor = true;
+            this.btnAzul.Click += new System.EventHandler(this.BtnAzul_Click);
+            // 
+            // btnVerde
+            // 
+            this.btnVerde.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerde.Location = new System.Drawing.Point(658, 130);
+            this.btnVerde.Name = "btnVerde";
+            this.btnVerde.Size = new System.Drawing.Size(75, 35);
+            this.btnVerde.TabIndex = 2;
+            this.btnVerde.Text = "Verde";
+            this.btnVerde.UseVisualStyleBackColor = true;
+            this.btnVerde.Click += new System.EventHandler(this.BtnVerde_Click);
+            // 
+            // btnRojo
+            // 
+            this.btnRojo.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRojo.Location = new System.Drawing.Point(658, 171);
+            this.btnRojo.Name = "btnRojo";
+            this.btnRojo.Size = new System.Drawing.Size(75, 33);
+            this.btnRojo.TabIndex = 3;
+            this.btnRojo.Text = "Rojo";
+            this.btnRojo.UseVisualStyleBackColor = true;
+            this.btnRojo.Click += new System.EventHandler(this.BtnRojo_Click);
+            // 
+            // btnNaranja
+            // 
+            this.btnNaranja.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNaranja.Location = new System.Drawing.Point(658, 210);
+            this.btnNaranja.Name = "btnNaranja";
+            this.btnNaranja.Size = new System.Drawing.Size(75, 38);
+            this.btnNaranja.TabIndex = 4;
+            this.btnNaranja.Text = "Naranja";
+            this.btnNaranja.UseVisualStyleBackColor = true;
+            this.btnNaranja.Click += new System.EventHandler(this.BtnNaranja_Click);
+            // 
+            // btnNegro
+            // 
+            this.btnNegro.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNegro.Location = new System.Drawing.Point(658, 254);
+            this.btnNegro.Name = "btnNegro";
+            this.btnNegro.Size = new System.Drawing.Size(75, 34);
+            this.btnNegro.TabIndex = 5;
+            this.btnNegro.Text = "Negro";
+            this.btnNegro.UseVisualStyleBackColor = true;
+            this.btnNegro.Click += new System.EventHandler(this.BtnNegro_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnNegro);
+            this.Controls.Add(this.btnNaranja);
+            this.Controls.Add(this.btnRojo);
+            this.Controls.Add(this.btnVerde);
+            this.Controls.Add(this.btnAzul);
+            this.Controls.Add(this.lblTitular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitular;
+        private System.Windows.Forms.Button btnAzul;
+        private System.Windows.Forms.Button btnVerde;
+        private System.Windows.Forms.Button btnRojo;
+        private System.Windows.Forms.Button btnNaranja;
+        private System.Windows.Forms.Button btnNegro;
+    }
+}
+
---
+++ Botones_de_color/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Botones_de_color
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btnAzul.ForeColor = Color.Blue;
+            btnVerde.ForeColor = Color.Green;
+            btnNaranja.ForeColor = Color.Orange;
+            btnRojo.ForeColor = Color.Red;
+            btnNegro.ForeColor = Color.Black;
+
+
+
+        }
+
+        private void BtnAzul_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Blue;
+        }
+
+        private void BtnVerde_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Green;
+        }
+
+        private void BtnRojo_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Red;
+                
+        }
+
+        private void BtnNaranja_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Orange;
+        }
+
+        private void BtnNegro_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Black;
+            lblTitular.ForeColor = Color.White;
+        }
+    }
+}
